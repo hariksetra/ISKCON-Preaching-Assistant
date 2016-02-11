c58607ac423a41950b6f0cf6333fa800dbb961f5
shyamsathyanathan <shyamsathyanathan@yahoo.in>
2016-02-11 20:51:11 +0530

    Removed old project and added Spring Boot project

---
+++ project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/PreachingAssistantApplication.java
@@ -0,0 +1,38 @@
+package com.giridhari.preachingassistant;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PreachingAssistantApplication {
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Value("${spring.datasource.username}")
+	private String username;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreachingAssistantApplication.class, args);
+	}
+    
+    @Bean
+    public DataSource dataSource() {
+    	DataSource ds = new DataSource();
+    	ds.setDriverClassName(driverClassName);
+    	ds.setUrl(url);
+    	ds.setUsername(username);
+    	ds.setPassword(password);
+    	
+    	return ds;
+    }
+}
