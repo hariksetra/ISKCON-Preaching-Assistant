944509638a0bf161b08838a284797d3daeb700ad
hariksetra <hariksetra.jps@gmail.com>
2016-05-14 18:10:08 +0530

    Added Content-Type header to allowed headers

--- project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/config/SimpleCORSFilter.java
+++ project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/config/SimpleCORSFilter.java
@@ -14,16 +14,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCORSFilter implements Filter {
-	
+
 	@Value("${cors.allowedOrigin}")
 	private String allowedOrigin;
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
         chain.doFilter(req, res);
     }
 
