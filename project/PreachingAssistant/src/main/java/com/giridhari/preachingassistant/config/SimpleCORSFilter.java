39da5f8f9a5adb25e7c6c9f4a54feb480cb6b687
shyamsathyanathan <shyamsathyanathan@yahoo.in>
2016-05-15 14:34:23 +0530

    Updated CORS headers and methods

--- project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/config/SimpleCORSFilter.java
+++ project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/config/SimpleCORSFilter.java
@@ -21,9 +21,9 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
         chain.doFilter(req, res);
     }
 
