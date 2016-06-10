2ce817933c0730f7d1fe199f57db97aecf1b0252
shyamsathyanathan <shyamsathyanathan@yahoo.in>
2016-06-10 12:56:52 +0530

    Implemented paging and sorting for Devotees

--- project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/db/repo/DevoteeRepo.java
+++ project/PreachingAssistant/src/main/java/com/giridhari/preachingassistant/db/repo/DevoteeRepo.java
@@ -1,11 +1,26 @@
 package com.giridhari.preachingassistant.db.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.giridhari.preachingassistant.db.model.Devotee;
 
 @Repository
-public interface DevoteeRepo extends CrudRepository<Devotee, Long> {
+public interface DevoteeRepo extends PagingAndSortingRepository<Devotee, Long> {
+	
+	@Query("select d from Devotee d")
+	public Page<Devotee> findAll(Pageable pageable);
+	
+	@Query("select distinct d from Devotee d where "
+			+ "d.initiatedName = ?1 or d.legalName = ?1 "
+			+ "or d.area = ?1 or d.smsPhone = ?1 "
+			+ "or d.designation = ?1")
+	public Page<Devotee> findByQuery(
+			@Param(value = "query") String query, 
+			Pageable pageable);
 	
 }
