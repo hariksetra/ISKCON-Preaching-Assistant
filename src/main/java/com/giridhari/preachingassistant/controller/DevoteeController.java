45bfe6372c14428da9f9bed0ae39e9495e15c928
Siddharth Dagar <dgr.siddharth@gmail.com>
2017-07-02 17:35:20 +0530

    Added PUT and POST for devotees

--- src/main/java/com/giridhari/preachingassistant/controller/DevoteeController.java
+++ src/main/java/com/giridhari/preachingassistant/controller/DevoteeController.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giridhari.preachingassistant.db.model.Devotee;
 import com.giridhari.preachingassistant.db.model.mapper.DevoteeMapper;
+import com.giridhari.preachingassistant.rest.model.devotee.DevoteeDetailRequestEntity;
 import com.giridhari.preachingassistant.rest.model.devotee.DevoteeDetailResponseEntity;
 import com.giridhari.preachingassistant.rest.model.devotee.DevoteeOverviewEntity;
 import com.giridhari.preachingassistant.rest.model.response.BaseDataResponse;
@@ -20,11 +22,11 @@ import com.giridhari.preachingassistant.service.DevoteeService;
 
 @RestController
 public class DevoteeController {
-	
+
 	@Resource
 	private DevoteeService devoteeService;
-	
-	@RequestMapping(name = "/devotees", method = RequestMethod.GET)
+
+	@RequestMapping(name = "/devotees", value="/devotees", method = RequestMethod.GET)
 	public BaseListReponse list() {
 		BaseListReponse response = new BaseListReponse();
 		List<Devotee> devoteeList = devoteeService.list();
@@ -36,12 +38,30 @@ public class DevoteeController {
 		response.setData(responseData);
 		return response;
 	}
-	
-	@RequestMapping(name = "/devotees/{id}", method = RequestMethod.GET)
-	public BaseDataResponse get(@RequestParam("id") long devoteeId) {
-		Devotee devotee = devoteeService.get(devoteeId);		
-		DevoteeDetailResponseEntity responseData = DevoteeMapper.convertToDevoteeDetailResponseEntity(devotee);		
+
+	@RequestMapping(name = "devoteeDetail", value="/devotees/{id}", method = RequestMethod.GET)
+	public BaseDataResponse get(@PathVariable("id") long devoteeId) {
+		Devotee devotee = devoteeService.get(devoteeId);
+		DevoteeDetailResponseEntity responseData = DevoteeMapper.convertToDevoteeDetailResponseEntity(devotee);
 		return new BaseDataResponse(responseData);
 	}
-	
+
+	@RequestMapping(name = "devoteeUpdate", value="/devotees/{id}", method = RequestMethod.PUT)
+	public DevoteeDetailResponseEntity put(@PathVariable("id") long devoteeId, @RequestBody DevoteeDetailRequestEntity requestData) {
+		Devotee devotee = devoteeService.get(devoteeId);
+		DevoteeMapper.patchDevotee(devotee, requestData);
+		devoteeService.update(devotee);
+		DevoteeDetailResponseEntity responseData = DevoteeMapper.convertToDevoteeDetailResponseEntity(devotee);
+		return responseData;
+	}
+
+	@RequestMapping(name="devoteeCreate", value="/devotees", method=RequestMethod.POST)
+	public DevoteeDetailResponseEntity post(@RequestBody DevoteeDetailRequestEntity requestData) {
+		Devotee devotee = new Devotee();
+		DevoteeMapper.patchDevotee(devotee, requestData);
+		devoteeService.update(devotee);
+		DevoteeDetailResponseEntity responseData = DevoteeMapper.convertToDevoteeDetailResponseEntity(devotee);
+		return responseData;
+	}
+
 }
--- src/main/java/com/giridhari/preachingassistant/db/model/mapper/DevoteeMapper.java
+++ src/main/java/com/giridhari/preachingassistant/db/model/mapper/DevoteeMapper.java
@@ -1,11 +1,12 @@
 package com.giridhari.preachingassistant.db.model.mapper;
 
 import com.giridhari.preachingassistant.db.model.Devotee;
+import com.giridhari.preachingassistant.rest.model.devotee.DevoteeDetailRequestEntity;
 import com.giridhari.preachingassistant.rest.model.devotee.DevoteeDetailResponseEntity;
 import com.giridhari.preachingassistant.rest.model.devotee.DevoteeOverviewEntity;
 
 public class DevoteeMapper {
-	
+
 	public static DevoteeDetailResponseEntity convertToDevoteeDetailResponseEntity(Devotee devotee) {
 		DevoteeDetailResponseEntity responseData = new DevoteeDetailResponseEntity();
 		responseData.setAddress(devotee.getAddress());
@@ -32,11 +33,12 @@ public class DevoteeMapper {
 		responseData.setPreferredLanguage(devotee.getPreferredLanguage());
 		responseData.setSikshaLevel(devotee.getSikshaLevel());
 		responseData.setSmsPhone(devotee.getSmsPhone());
-		responseData.setUserAccountId(devotee.getUserAccount().getId());
-		
+		if (devotee.getUserAccount() != null)
+			responseData.setUserAccountId(devotee.getUserAccount().getId());
+
 		return responseData;
 	}
-	
+
 	public static DevoteeOverviewEntity convertToDevoteeOverviewEntity(Devotee devotee) {
 		DevoteeOverviewEntity devoteeOverviewEntity = new DevoteeOverviewEntity();
 		devoteeOverviewEntity.setId(devotee.getId());
@@ -44,8 +46,88 @@ public class DevoteeMapper {
 		devoteeOverviewEntity.setPhone(devotee.getSmsPhone());
 		devoteeOverviewEntity.setIntroDate(devotee.getIntroDate());
 		devoteeOverviewEntity.setPreferredLanguage(devotee.getPreferredLanguage());
-		
+
 		return devoteeOverviewEntity;
 	}
+
+	public static void patchDevotee(Devotee devotee, DevoteeDetailRequestEntity requestData) {
+		if (requestData.getLegalName() != null) {
+			devotee.setLegalName(requestData.getLegalName());
+		}
+
+		if (requestData.getInitiatedName() != null)
+			devotee.setInitiatedName(requestData.getInitiatedName());
+
+		if (requestData.getAddress() != null)
+			devotee.setAddress(requestData.getAddress());
+
+		if (requestData.getArea() != null)
+			devotee.setArea(requestData.getArea());
+
+		if (requestData.getCapturedBy() != null) {
+			Devotee capturedBy = new Devotee();
+			capturedBy.setId(requestData.getCapturedBy());
+			devotee.setCapturedBy(capturedBy);
+		}
+
+		if (requestData.getCapturedFor() != null)
+			devotee.setCapturedFor(requestData.getCapturedFor());
+
+		if (requestData.getDescription() != null)
+			devotee.setDescription(requestData.getDescription());
+
+		if (requestData.getDesignation() != null)
+			devotee.setDesignation(requestData.getDesignation());
+
+		if (requestData.getDob() != null)
+			devotee.setDob(requestData.getDob());
+
+		if (requestData.getEducation() != null)
+			devotee.setEducation(requestData.getEducation());
+
+		if (requestData.getEmail() != null)
+			devotee.setEmail(requestData.getEmail());
+
+		if (requestData.getFamilyInfo() != null)
+			devotee.setFamilyInfo(requestData.getFamilyInfo());
+
+		if (requestData.getGender() != null)
+			devotee.setGender(requestData.getGender());
+
+		if (requestData.getIncomeScale() != null)
+			devotee.setIncomeScale(requestData.getIncomeScale());
+
+		if (requestData.getInitiatedName() != null)
+			devotee.setInitiatedName(requestData.getInitiatedName());
+
+		if (requestData.getIntroDate() != null)
+			devotee.setIntroDate(requestData.getIntroDate());
+
+		if (requestData.getLegalName() != null)
+			devotee.setLegalName(requestData.getLegalName());
+
+		if (requestData.getMaritalStatus() != null)
+			devotee.setMaritalStatus(requestData.getMaritalStatus());
+
+		if (requestData.getMonthlyContribution() != null)
+			devotee.setMonthlyContribution(requestData.getMonthlyContribution());
+
+		if (requestData.getOccupation() != null)
+			devotee.setOccupation(requestData.getOccupation());
+
+		if (requestData.getOrganization() != null)
+			devotee.setOrganization(requestData.getOrganization());
+
+		if (requestData.getPreferredLanguage() != null)
+			devotee.setPreferredLanguage(requestData.getPreferredLanguage());
+
+		if (requestData.getSikshaLevel() != null)
+			devotee.setSikshaLevel(requestData.getSikshaLevel());
+
+		if (requestData.getSmsPhone() != null)
+			devotee.setSmsPhone(requestData.getSmsPhone());
+
+	}
+
 
 }
